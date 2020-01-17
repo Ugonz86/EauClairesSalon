2d507118a6b69c861a265432ca82d242482bb185
Ugonz86 <ugonzalez86@gmail.com>
2020-01-17 11:50:12 -0800

    Add Details method on both controllers

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -39,5 +39,11 @@ namespace HairSalon.Controllers
             return RedirectToAction("Index");
         }
 
+        public ActionResult Details(int id)
+        {
+        Client thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+        return View(thisClient);
+        }
+
     }
 }
